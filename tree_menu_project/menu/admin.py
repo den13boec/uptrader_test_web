b6d9cacd8636e7618e98166136f58e721d30c703
den13boec <den-boris@list.ru>
2025-05-15 23:29:36 +0300

    Add admin setup

--- tree_menu_project/menu/admin.py
+++ tree_menu_project/menu/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import MenuItem
 
-# Register your models here.
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'menu_name', 'parent', 'slug')
+    search_fields = ('name',)
+    list_filter = ('menu_name',)
+
+admin.site.register(MenuItem, MenuItemAdmin)
