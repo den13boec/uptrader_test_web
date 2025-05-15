2f222d26dee0a00db6ec52649d15c02d076081d1
den13boec <den-boris@list.ru>
2025-05-15 22:45:52 +0300

    Add model for menu and initial migration

--- tree_menu_project/menu/models.py
+++ tree_menu_project/menu/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+    parent = models.ForeignKey(
+        'self', on_delete=models.CASCADE, blank=True, null=True, related_name='children'
+    )
+    menu_name = models.CharField(max_length=50)
+    named_url = models.CharField(max_length=200, blank=True, null=True)
+    explicit_url = models.CharField(max_length=200, blank=True, null=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['slug', 'menu_name'], name='unique_slug_per_menu')
+        ]
+
+    def __str__(self):
+        return self.name
+
+    def get_url(self):
+        if self.named_url:
+            try:
+                return reverse(self.named_url)
+            except:
+                return '#'
+        elif self.explicit_url:
+            return self.explicit_url
+        return '#'
--- tree_menu_project/tree_menu_project/settings.py
+++ tree_menu_project/tree_menu_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8_@rsiofo=saii*@r#8)5inw088gfda7sbm=n4@d)qrx4mc1tw'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
